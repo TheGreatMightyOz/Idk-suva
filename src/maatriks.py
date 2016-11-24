0022ce8ab24c1b1c149a47377ed6177b1d7992a3
TheGreatMightyOz <oskasoop@gmail.com>
2016-11-24 13:41:15 +0200

    Update maatriks.py

--- src/maatriks.py
+++ src/maatriks.py
@@ -6,6 +6,7 @@ import inverse
 #main
 print("Sisesta operatsioon, mida soovid sooritada.")
 print("Lisainfo saamiseks kirjuta \"help\".")
+maatrixid=[]
 while True:
     a=input("> ").lower()
     if a in {"exit"}:
@@ -13,11 +14,23 @@ while True:
     elif a in {"help"}:
         print("No help")
     elif a in {"determinant", "det"}:
-        print("Denis")
+        try:
+            print(determinant.determinant(maatrixid[len(maatrixid)-1]))
+        except IndexError:
+            print("Peab leiduma maatriks millest determinanti võtta")
     elif a in {"korrutis","korruta","kor"}:
-        print("koer")
+        try:
+            print(korrutis.korrutis(maatrixid[0],maatrixid[1]))
+        except IndexError:
+            print("Peab olema kaks maatriksit mida korrutada.")
     elif a in {"pöördmaatriks", "inverse", "inv"}:
         print("Siin me näeme")
     elif a in {"sin"}:
         print("")
-        
+    elif a in {"file"}:
+        while(True):
+            fileName=input("Sisesta faili nimi\n>")
+            f=fileinput.get_file(fileName)
+            if(f!=None):
+                maatrixid.append(f)
+                break

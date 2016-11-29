39677d83fcfe2e99c5736504b5d4d7c69ee4532c
TheGreatMightyOz <oskasoop@gmail.com>
2016-11-29 14:34:50 +0200

    Update fileinput.py

--- src/fileinput.py
+++ src/fileinput.py
@@ -2,8 +2,7 @@ def get_file(filename):
     try:
         f = open(filename, 'r')
         lines = f.readlines()
-        cols, rows = (int(val) for val in lines[0].split())
-        return [[int(val) for val in line.split()] for line in lines[:]]
+        return [[float(val) for val in line.split()] for line in lines[:]]
     except IOError:
         print(filename+" is missing.")
         
